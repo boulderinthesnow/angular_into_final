3784a6e9fe587dde5f26cfb937cb8b1ee56b7a0a
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-12 12:09:31 -0700

    working on having form populate on page

--- 8-12-15/app.js
+++ 8-12-15/app.js
@@ -1,10 +1,20 @@
 var app = angular.module("firstApp", []);
 
-
+	
 	app.controller("clickController", function($scope){
+		var places = [];
 		var showForm = false
 		$scope.newPost = function() {
 			$scope.showForm = true
 		}
-		
+		$scope.saveForm = function(form) {
+			$scope.showForm = false;
+			// add data from post into an object and push it into places array
+			places.push({
+				title: form.title,
+				author: form.author,
+				image: form.image,
+				description: form.description
+			})
+			}
 	})
