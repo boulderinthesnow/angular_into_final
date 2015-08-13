235d2d19e3e92fad89df834b978feb631e852dd2
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-12 17:16:29 -0700

    sorting places working

--- 8-12-15/app.js
+++ 8-12-15/app.js
@@ -1,12 +1,34 @@
-var app = angular.module("firstApp", []);
+var app = angular.module("firstApp", [
+	'angularMoment'
+	]);
 
 	
 	app.controller("clickController", function($scope){
-		$scope.places = [];
+		$scope.places = [
+							{
+								title: "Lake Tahoe", 
+								author: "Matthew Williams", 
+								image:"http://www.ironman.com/~/media/7b907617deea480a8b6a109d898f75e4/lake%20tahoe%20carousel2.jpg?w=1600&h=980&c=1", 
+								description:" Wolf bespoke before they sold out, narwhal deep v craft beer paleo put a bird on it. Kickstarter swag cronut Schlitz pork belly. Cold-pressed sustainable irony, tattooed letterpress American Apparel brunch. Keytar literally crucifix heirloom +1, craft beer dreamcatcher fanny pack paleo brunch raw denim church-key fashion axe authentic. Letterpress gentrify cold-pressed, hella stumptown twee Odd Future. Vice Pinterest Portland Brooklyn. Pinterest organic polaroid retro.", 
+								time: new Date(),
+								rank: 0,
+								count: 0
+							},
+							{
+								title: "Lake Tahoe", 
+								author: "Matthew Williams", 
+								image:"http://www.ironman.com/~/media/7b907617deea480a8b6a109d898f75e4/lake%20tahoe%20carousel2.jpg?w=1600&h=980&c=1", 
+								description:" Wolf bespoke before they sold out, narwhal deep v craft beer paleo put a bird on it. Kickstarter swag cronut Schlitz pork belly. Cold-pressed sustainable irony, tattooed letterpress American Apparel brunch. Keytar literally crucifix heirloom +1, craft beer dreamcatcher fanny pack paleo brunch raw denim church-key fashion axe authentic. Letterpress gentrify cold-pressed, hella stumptown twee Odd Future. Vice Pinterest Portland Brooklyn. Pinterest organic polaroid retro.", 
+								time: new Date(),
+								rank: 0,
+								count: 1
+							}
+						];
 		var showForm = false
 		$scope.newPost = function() {
 			$scope.showForm = true
 		}
+		$scope.count = 1;
 		$scope.saveForm = function(form) {
 			console.log(form);
 			$scope.showForm = false;
@@ -15,7 +37,17 @@ var app = angular.module("firstApp", []);
 				title: form.title,
 				author: form.author,
 				image: form.image,
-				description: form.description
+				description: form.description,
+				time: new Date(),
+				rank: 0,
+				count: $scope.count += 1
 			})
+			count += 1
+		}
+		$scope.up = function(count) {
+			$scope.places[count].rank += 1
+		}
+		$scope.down = function(count) {
+			$scope.places[count].rank -= 1
 		}
 	})
