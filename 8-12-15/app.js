6fa8b4c15d229f9c8c2274568d7a2f21d4fa35a5
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-12 20:21:49 -0700

    form validation for title working w errors

--- 8-12-15/app.js
+++ 8-12-15/app.js
@@ -17,7 +17,7 @@ var app = angular.module("firstApp", [
 								title: "Lake WoeBeGone", 
 								author: "Matthew Williams", 
 								image:"http://www.ironman.com/~/media/7b907617deea480a8b6a109d898f75e4/lake%20tahoe%20carousel2.jpg?w=1600&h=980&c=1", 
-								description:" Wolf bespoke before they sold out, narwhal deep v craft beer paleo put a bird on it. Kickstarter swag cronut Schlitz pork belly. Cold-pressed sustainable irony, tattooed letterpress American Apparel brunch. Keytar literally crucifix heirloom +1, craft beer dreamcatcher fanny pack paleo brunch raw denim church-key fashion axe authentic. Letterpress gentrify cold-pressed, hella stumptown twee Odd Future. Vice Pinterest Portland Brooklyn. Pinterest organic polaroid retro.", 
+								description:"DIY post-ironic Helvetica cred. Lo-fi lomo direct trade keytar meh put a bird on it. Typewriter Helvetica American Apparel, butcher lo-fi dreamcatcher migas. Ethical seitan Intelligentsia, photo booth brunch cornhole messenger bag. Tilde sustainable iPhone Godard heirloom tattooed Brooklyn, disrupt fap. Pitchfork bitters 3 wolf moon stumptown letterpress. Letterpress normcore butcher tousled retro pour-over.", 
 								time: new Date(),
 								rank: 0
 							}
@@ -27,17 +27,18 @@ var app = angular.module("firstApp", [
 			$scope.showForm = true
 		}
 		$scope.saveForm = function(form) {
-			console.log(form);
-			$scope.showForm = false;
-			// DONE add data from post into an object and push it into places array
-			$scope.places.push({
-				title: form.title,
-				author: form.author,
-				image: form.image,
-				description: form.description,
-				time: new Date(),
-				rank: 0
-			})
+			if ($scope.placeForm.$valid) {
+				$scope.showForm = false;
+				// DONE add data from post into an object and push it into places array
+				$scope.places.push({
+					title: form.title,
+					author: form.author,
+					image: form.image,
+					description: form.description,
+					time: new Date(),
+					rank: 0
+				})
+			}
 		}
 		$scope.up = function(place) {
 			place.rank += 1
