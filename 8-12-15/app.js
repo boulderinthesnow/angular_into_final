66ed4aa559199f9ed9b6714b114871417b1cfead
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-13 11:12:49 -0700

    comments adding to correct arrays

--- 8-12-15/app.js
+++ 8-12-15/app.js
@@ -12,7 +12,8 @@ var app = angular.module("firstApp", [
 								description:" Wolf bespoke before they sold out, narwhal deep v craft beer paleo put a bird on it. Kickstarter swag cronut Schlitz pork belly. Cold-pressed sustainable irony, tattooed letterpress American Apparel brunch. Keytar literally crucifix heirloom +1, craft beer dreamcatcher fanny pack paleo brunch raw denim church-key fashion axe authentic. Letterpress gentrify cold-pressed, hella stumptown twee Odd Future. Vice Pinterest Portland Brooklyn. Pinterest organic polaroid retro.", 
 								time: new Date(),
 								rank: 0,
-								comments: {}
+								showComment: false,
+								comments: []
 
 							},
 							{
@@ -22,7 +23,8 @@ var app = angular.module("firstApp", [
 								description:"DIY post-ironic Helvetica cred. Lo-fi lomo direct trade keytar meh put a bird on it. Typewriter Helvetica American Apparel, butcher lo-fi dreamcatcher migas. Ethical seitan Intelligentsia, photo booth brunch cornhole messenger bag. Tilde sustainable iPhone Godard heirloom tattooed Brooklyn, disrupt fap. Pitchfork bitters 3 wolf moon stumptown letterpress. Letterpress normcore butcher tousled retro pour-over.", 
 								time: new Date(),
 								rank: 0,
-								comments: {}
+								showComment: false,
+								comments: []
 							}
 						];
 		var showForm = false
@@ -32,7 +34,6 @@ var app = angular.module("firstApp", [
 		$scope.saveForm = function(form) {
 			if ($scope.placeForm.$valid) {
 				$scope.showForm = false;
-				// DONE add data from post into an object and push it into places array
 				$scope.places.push({
 					title: form.title,
 					author: form.author,
@@ -40,12 +41,21 @@ var app = angular.module("firstApp", [
 					description: form.description,
 					time: new Date(),
 					rank: 0,
-					comments: {}
+					showComment: false,
+					comments: []
 				})
 			}
 		}
-		$scope.newComment = function() {
-			$scope.showComment = true;
+		$scope.newComment = function(place) {
+				place.showComment = true;
+		}
+		$scope.saveComment = function(comment, place) {
+				place.showComment = false;
+				place.comments.push({
+					author: comment.author,
+					comment: comment.text
+				})
+				console.log(place.comments.length)
 		}
 		$scope.up = function(place) {
 			place.rank += 1
