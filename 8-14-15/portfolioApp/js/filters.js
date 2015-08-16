37e3b4df04305f63d02d240148f11f689f972d15
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-15 19:11:55 -0700

    camel case filter

--- 8-14-15/portfolioApp/js/filters.js
+++ 8-14-15/portfolioApp/js/filters.js
@@ -26,3 +26,21 @@ app.filter('camel', function () {
  	}
 })
 
+app.filter('pigLatin', function () {
+ return function (input) {
+  	// get the index of the underscore
+  	// run to uppercase on the char after that index
+ 		if (typeof(input) === "string") {
+ 			 	input = input.replace(/_[a-z]/g, function (g) {
+ 			 		return g[1].toUpperCase()
+ 			 	})
+ 			 	input = input.replace(/ [a-z]/g, function (g) {
+ 			 		return g[1].toUpperCase()
+ 			 	})
+ 			return input
+ 		} else {
+ 			return (input)
+ 		}
+ 	}
+})
+
--- temp/kebab.js
+++ temp/kebab.js
@@ -2,10 +2,7 @@
  	// get the index of the underscore
  	// run to uppercase on the char after that index
 		if (typeof(input) === "string") {
-			 	input = input.replace(/_[a-z]/g, function (g) {
-			 		return g[1].toUpperCase()
-			 	})
-			 	input = input.replace(/-[a-z]/g, function (g) {
+			 	input = input.replace(/ [a-z]/g, function (g) {
 			 		return g[1].toUpperCase()
 			 	})
 			return input
