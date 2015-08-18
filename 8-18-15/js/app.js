17abb1077f6e44e1aefd2ac5738b9b4bacb03219
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-18 15:48:26 -0700

    movie search working with colors

--- 8-18-15/js/app.js
+++ 8-18-15/js/app.js
@@ -3,7 +3,7 @@ app.config(function($routeProvider, $httpProvider) {
     // $httpProvider.defaults.withCredentials = true;
     $routeProvider
       .when('/', {
-        templateUrl: '/partials/results.html',
+        templateUrl: '/partials/show.html',
         controller: 'MovieController'
       })
   })
--- 8-18-15/js/controllers.js
+++ 8-18-15/js/controllers.js
@@ -1,15 +1,17 @@
 app.controller("MovieController", ['$scope', '$location', '$http', function($scope, $location, $http){
-	$scope.chat = "Chat!"
-	$scope.url = $location.url();
-	
+
 	$scope.searchDb = function () {
-		var searchUrl = 'http://www.omdbapi.com/?t=' + $scope.searchData
+		var searchUrl = 'http://www.omdbapi.com/?s=' + $scope.searchData
+		// $location.path('/')
+		console.log(searchUrl)
 		$http.get(searchUrl).then(function(res) {
-			$scope.movieResults = res.data
+			$scope.movieResults = res.data.Search
 			console.log($scope.movieResults)
+			
 		}, function(response) {
 			console.log("Houston, we have a problem")
 		})
 	}
-
 }]);
+
+// encodeURIComponent($routeParams)
