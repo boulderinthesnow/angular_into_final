cadd4c0352482d7d01be90b47e0dea3e72109462
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-18 09:56:16 -0700

    chat post in progress

--- 8-17-15/js/controllers.js
+++ 8-17-15/js/controllers.js
@@ -1,10 +1,22 @@
 app.controller("ChatController", ['$scope', '$location', '$http', function($scope, $location, $http){
 	$scope.chat = "Chat!"
 	$scope.url = $location.url();
+
 	$http.get('https://shielded-peak-6345.herokuapp.com/').then(function(data) {
 		$scope.chats = data
 		console.log($scope.chats)
 	}, function(response) {
 		console.log("Houston, we have a problem")
 	})
+
+	$http.post('https://shielded-peak-6345.herokuapp.com/', {message:{
+		name: "Donald Trump",
+		content: "if you get in my way there will be hell toupee!"
+	}}).then(function(data) {
+		$scope.sent = data
+		console.log($scope.sent)
+	}, function(response) {
+		console.log("Houston, we have a problem")
+	})
+
 }])
