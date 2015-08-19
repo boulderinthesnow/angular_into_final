229a9c4fecb4cd97d004fbcb20090ef8884ba0ba
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-18 18:10:01 -0700

    search data in path

--- 8-18-15/js/controllers.js
+++ 8-18-15/js/controllers.js
@@ -1,7 +1,20 @@
 app.controller("MovieController", ['$scope', '$location', '$http', function($scope, $location, $http){
 
+	$scope.showMovie = function (title) {
+		$location.path('/movie')
+		console.log(title)
+		var searchUrl = 'http://www.omdbapi.com/?t=' + encodeURIComponent(title)
+		$http.get(searchUrl).then(function(res) {
+			$scope.movieResults = res.data.Search
+			console.log($scope.movieResults)
+			
+		}, function(response) {
+			console.log("Houston, we have a problem")
+		})
+	}
+
 	$scope.searchDb = function () {
-		var searchUrl = 'http://www.omdbapi.com/?s=' + $scope.searchData
+		var searchUrl = 'http://www.omdbapi.com/?s=' + encodeURIComponent($scope.searchData)
 		// $location.path('/')
 		console.log(searchUrl)
 		$http.get(searchUrl).then(function(res) {
@@ -13,17 +26,16 @@ app.controller("MovieController", ['$scope', '$location', '$http', function($sco
 		})
 	}
 
-	$scope.showMovie = function (title) {
-		// $location.path('/')
-		console.log(title)
-		$http.get(searchUrl).then(function(res) {
-			$scope.movieResults = res.data.Search
-			console.log($scope.movieResults)
-			
-		}, function(response) {
-			console.log("Houston, we have a problem")
-		})
-	}
 }]);
 
 // encodeURIComponent($routeParams)
+
+app.controller("SearchController", ['$scope', '$location', '$http', function($scope, $location, $http){
+
+	console.log($scope.searchData)
+	$scope.searchDb = function () {
+		$location.path('/show/' + $scope.searchData)
+	}
+
+}]);
+
