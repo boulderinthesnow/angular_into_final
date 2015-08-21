fd62743c3e5cae36fa5859e8317be93f2bf9b481
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-21 15:22:08 -0700

    teas appearing the view

--- 8-21-15/js/controllers.js
+++ 8-21-15/js/controllers.js
@@ -2,16 +2,9 @@ app.controller("ShowController", ['$scope', '$http', function($scope, $http){
 	 var showAllTea = function () {
 		$http.get('http://localhost:8000/json/data.json').then(function(data) {
 			$scope.teaArray = data.data
-			console.log($scope.teaArray)
 		}
 		, function(response) {
 			console.log("ruh roh...")
-		})	
+		})	// END FUNCTION
 	}() // END FUNCTION
-}])
-
-
-// $http.get('http://localhost:8000/json/itunes.json').then(function(data) {
-// 	$scope.songs = data.data.results
-// 	console.log($scope.songs)
-// })
+}]) // END CONTROLLER
