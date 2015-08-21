26a773f06bf991177f537905c8ee3e498f235a46
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-21 15:30:56 -0700

    tea pics added

--- 8-21-15/js/controllers.js
+++ 8-21-15/js/controllers.js
@@ -8,3 +8,10 @@ app.controller("ShowController", ['$scope', '$http', function($scope, $http){
 		})	// END FUNCTION
 	}() // END FUNCTION
 }]) // END CONTROLLER
+
+
+// {"_id":"55c8ee82152165d244b98300","name":"Bayard stew",
+// "ingredients":"concentrated gluten, jewelry, dill, beetle nut, toast",
+// "caffeineScale":244,"price":1540,"inStock":true,"rating":1,
+// "imageUrl":"http://s7d5.scene7.com/is/image/Teavana/32664_d?$cimg$",
+// "__v":0,"categories":["dark","cold"]}
