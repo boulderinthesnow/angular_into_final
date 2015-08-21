9d9c7cfdbd4674cadea1ca94c745df2ff9e5b54d
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-21 14:28:47 -0700

    JSON added

--- 8-21-15/js/controllers.js
+++ 8-21-15/js/controllers.js
@@ -1,3 +1,17 @@
 app.controller("ShowController", ['$scope', '$http', function($scope, $http){
-	
+	// function showAllTea() {
+		$http.get('http://localhost:8000/json/data.json').then(function(data) {
+			console.log(data, "FOO")
+		})
+		// , function(response) {
+		// 	console.log("ruh roh...")
+		// })
+		
+	// }() // END FUNCTION
 }])
+
+
+// $http.get('http://localhost:8000/json/itunes.json').then(function(data) {
+// 	$scope.songs = data.data.results
+// 	console.log($scope.songs)
+// })
