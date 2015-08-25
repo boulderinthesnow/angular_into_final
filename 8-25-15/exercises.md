185251f15eca41b4735effde142816bf3c383e23
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-25 16:41:02 -0700

    yoyo app completed

--- 8-25-15/exercises.md
+++ 8-25-15/exercises.md
@@ -1,4 +1,4 @@
 ####EXERCISE
 ####What is accessible in the directive's scope? How is data being passed to the directive? What is different about the gs-yoyo-details element from the first example?
 yoyoData is accessable through the variable yoyo. Data is being passed to the directive from the YoyoController when it renders the yoyo-details html page
-In the first example the yoyo variable was contained soley inside the Yoyo Controller. If we needed to use the yoyo variable inside another Controller, we'd be in trouble. The second syntax allows the variable to be be accessable whenever the gsYoyoDetails directive is called.
+In the first example the yoyo variable was contained soley inside the Yoyo Controller. If we needed to use the yoyo variable inside another Controller, we'd be in trouble. The second syntax allows the variable to be.
--- 8-25-15/js/app.js
+++ 8-25-15/js/app.js
@@ -1,16 +1,23 @@
 var app = angular.module('yoyoDirectiveApp', [])
 
 app.controller('YoyoController', ['$scope', function($scope) {
-  $scope.yoyo = {name: 'Duncan Metal Drifter',
-    img: "http://www.toysrus.com/graphics/tru_prod_images/Duncan-Metal-Drifter-Pro-Yo-Yo--pTRU1-8444206dt.jpg"
-  };
+  $scope.yoyos = [{name: 'Duncan Metal Drifter',
+			    img: "http://www.toysrus.com/graphics/tru_prod_images/Duncan-Metal-Drifter-Pro-Yo-Yo--pTRU1-8444206dt.jpg"},
+			    {name: 'Bad Ass',
+			    img: "http://yoyobestbuy.com/wp-content/uploads/Bad-Ass-3.jpg"},
+			    {name: 'Pretty yoyo',
+			    img: "https://s-media-cache-ak0.pinimg.com/736x/72/e1/fa/72e1fa89c1efea5ea12e719fae83efa4.jpg"}
+				];
+				console.log($scope.yoyos[0].name)
 }]);
 
 app.directive('gsYoyoDetails', function() {
   return {
     templateUrl: '../partials/yoyo-details.html',
     scope: {
-    	yoyo: '=yoyoData'
+    	yoyos: '=yoyoData'
     }
   };
 });
+
+
