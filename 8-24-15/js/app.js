64aebd64e81831db0d4e53f7320dc165a76567ac
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-25 10:37:22 -0700

    custom directives started

--- 8-24-15/js/app.js
+++ 8-24-15/js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('simpleDirectiveApp', [])
 app.directive('gsAngularLogo', function() {
   return {
   	restrict: "E",
-    template: '<img src="https://lh6.googleusercontent.com/-TlY7amsfzPs/T9ZgLXXK1cI/AAAAAAABK-c/Ki-inmeYNKk/w749-h794/AngularJS-Shield-large.png">'
+    templateUrl: 'partials/tim.html'
   };
 });
 
