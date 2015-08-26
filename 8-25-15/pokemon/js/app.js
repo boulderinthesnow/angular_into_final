70a3430391c357017e438e778924b41d38dfe8d6
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-26 11:32:50 -0700

    pokemon obj created' '

--- 8-25-15/pokemon/js/app.js
+++ 8-25-15/pokemon/js/app.js
@@ -1,29 +1,63 @@
 var app = angular.module('pokemonApp', [])
 
-app.controller('PokeController', ['$scope', '$http', function($scope, $http) {
+app.controller('PokeController', ['$scope', '$http', '$q', function($scope, $http, $q) {
+	$scope.pokeArr = [];
 	$http.get('http://pokeapi.co/api/v1/pokedex/1/').
 		then(function (response) {
 			$scope.pokemons = response.data.pokemon
+			$scope.CompleteArr = fiveRandPoke($scope.pokemons)
+			// console.log($scope.pokeArr)
 		}, function (response) {
 			console.log("Ruh Roh")
 		});
 
-  // $scope.pokemons = [{name: 'Duncan Metal Drifter',
-		// 	    img: "http://www.toysrus.com/graphics/tru_prod_images/Duncan-Metal-Drifter-Pro-Yo-Yo--pTRU1-8444206dt.jpg"},
-		// 	    {name: 'Bad Ass',
-		// 	    img: "http://yoyobestbuy.com/wp-content/uploads/Bad-Ass-3.jpg"},
-		// 	    {name: 'Pretty yoyo',
-		// 	    img: "https://s-media-cache-ak0.pinimg.com/736x/72/e1/fa/72e1fa89c1efea5ea12e719fae83efa4.jpg"}
-		// 		];
-		// 		console.log($scope.pokemons[0].name)
-}]);
+		var fiveRandPoke = function (data) {
+			// console.log(data, 'data')
+			arr = [];
+			for (i = 0 ; arr.length < 5 ; i++) {
+				var fivePoke = Math.floor(Math.random() * ($scope.pokemons.length -10))	
+				if (fivePoke < 700) { 
+					arr.push(fivePoke)
+				}
+			} // END FOR LOOP
+
+			$q.all([
+				$http.get('http://pokeapi.co/api/v1/pokemon/' + arr[0] + '/'),
+				$http.get('http://pokeapi.co/api/v1/pokemon/' + arr[1] + '/'),
+				$http.get('http://pokeapi.co/api/v1/pokemon/' + arr[2] + '/'),
+				$http.get('http://pokeapi.co/api/v1/pokemon/' + arr[3] + '/'),
+				$http.get('http://pokeapi.co/api/v1/pokemon/' + arr[4] + '/') 
+			]).then( function (arrayOfResponses) {
+				arrayOfResponses.forEach(function (pokemon) {
+					var tempObj = {};
+					pokemon = pokemon.data;
+					tempObj.name = pokemon.name;
+					
+					//GET IMAGE
+					var spriteAddress = ('http://pokeapi.co' + pokemon.sprites[0].resource_uri)
+					var tempImage = parseInt((spriteAddress.split("/")[6])) - 1
+					tempObj.image = 'http://pokeapi.co/media/img/' + tempImage + '.png'
+					//PUSH TO ARRAY
+					$scope.pokeArr.push(tempObj)
+				})
+				console.log($scope.pokeArr)
+				// return ($scope.pokeArr)
+			})
+		} // END FUNCTION	
+	}]);
+
 
 
 app.directive('gsPokemonDetails', function() {
 	return {
 		templateUrl: '../partials/show-pokemon.html',
 		scope: {
-			pokemons: '=pokemonData'
+			pokemons: '='
 		}
 	}
 }); 
+
+
+// The app should display the pokemon's name, types, name of moves 
+// (limit it to 6), and a sprite for the pokemon. Use a custom 
+// directive to display the pokemon.
