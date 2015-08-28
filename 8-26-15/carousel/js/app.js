948b1d189d9e4e67298555b3bb160feb903bbc44
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-27 18:47:01 -0700

    carousel working

--- 8-26-15/carousel/js/app.js
+++ 8-26-15/carousel/js/app.js
@@ -8,12 +8,33 @@ app.directive('gsCarouselMaker', function() {
   return {
     restrict: 'E',
     link: function(scope, element, attrs) {
+      var count = 0
+      
       scope.scrollRight = function (param) {
-        console.log("scroll right clicked", param)
+        console.log("scroll right clicked", count)
+        scope.image = param[count]
+        count++
+        if (count > 2) {
+         count = 0
+        } 
       };
+
+      scope.scrollLeft = function (param) {
+
+        console.log("scroll left clicked", count)
+        scope.image = param[count]
+        count--
+
+        if (count < 0) {
+         count = 2;
+        }
+      };
+
        // scope.right = function(array) {
        //  array.push ("foo") 
        // }
+
+
     },
     templateUrl: '../partials/carousel.html',
     scope: {
