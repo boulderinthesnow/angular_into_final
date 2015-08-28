5210ab37ec68109620b5aac7575d610adbd0c72e
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-28 15:34:09 -0700

    carousel fixed

--- 8-26-15/carousel/js/app.js
+++ 8-26-15/carousel/js/app.js
@@ -1,34 +1,33 @@
 var app = angular.module('carouselApp', [])
 
-app.controller('CarouselController', ["$scope", function($scope){
-}])
-
 app.directive('gsCarouselMaker', function() {
   return {
     restrict: 'E',
     link: function(scope, element, attrs) {
+      
       scope.image = scope.imageArray[0]
       var count = 0
       scope.scrollRight = function (param) {
-        count++
+        console.log(count)
         scope.image = param[count]
+        count++
         if (count > param.length - 1) {
          count = 0
         } 
       };
 
       scope.scrollLeft = function (param) {
-        count--
+        console.log(count)
         scope.image = param[count]
+        count--
         if (count < 0) {
-         count = 2;
+         count = param.length - 1;
         }
       };
-
     },
     templateUrl: '../partials/carousel.html',
     scope: {
-      imageArray: '=' 
+      imageArray: '=',
     }
   };
 });
