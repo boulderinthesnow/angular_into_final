2bb3bedfb394c49010d65985e7d01d23650b319e
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-29 15:56:59 -0700

    converter working

--- 8-28-15/js/app.js
+++ 8-28-15/js/app.js
@@ -1,8 +1,12 @@
 var app = angular.module("coinApp", ["firebase"]);
   app.controller("CurrencyController", ["$scope", "$firebaseObject", function($scope, $firebaseObject) {
     var bitcoinRef = new Firebase("https://publicdata-cryptocurrency.firebaseio.com/bitcoin")
-    console.log(bitcoinRef)
+    var liteRef = new Firebase("https://publicdata-cryptocurrency.firebaseio.com/litecoin")
+    var dogeRef = new Firebase("https://publicdata-cryptocurrency.firebaseio.com/dogecoin")
    $scope.bitcoinData = $firebaseObject(bitcoinRef)
+   $scope.liteData = $firebaseObject(liteRef)
+   $scope.dogeData = $firebaseObject(dogeRef)
+
   }
 ]);
   
