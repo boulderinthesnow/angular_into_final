4d15c631c1f369db071ddc5b7307f9bbc71f8a66
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-29 15:24:33 -0700

    bitcoin appearing on page

--- 8-28-15/js/app.js
+++ 8-28-15/js/app.js
@@ -1,9 +1,8 @@
 var app = angular.module("coinApp", ["firebase"]);
   app.controller("CurrencyController", ["$scope", "$firebaseObject", function($scope, $firebaseObject) {
-    var bitcoinRef = new firebase("https://publicdata-cryptocurrency.firebaseio.com/bitcoin")
+    var bitcoinRef = new Firebase("https://publicdata-cryptocurrency.firebaseio.com/bitcoin")
     console.log(bitcoinRef)
    $scope.bitcoinData = $firebaseObject(bitcoinRef)
   }
 ]);
-
-console.log("foo")  
+  
