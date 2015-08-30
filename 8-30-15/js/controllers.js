32898271b44dc80700f0984d7c200a203978a230
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-30 14:22:34 -0700

    avatar and destroy working

--- 8-30-15/js/controllers.js
+++ 8-30-15/js/controllers.js
@@ -1,7 +1,19 @@
 app.controller('ChatController', function($scope, $firebaseArray){
 	var chatRef = new Firebase("https://radiant-torch-6315.firebaseio.com/chat")
-	$scope.chat = $firebaseArray(chatRef)
-	console.log($scope.chat)
+	$scope.chats = $firebaseArray(chatRef)
+	$scope.newChat = {name: "", avatar:"", text: ""}
+
+	$scope.addChat = function() {
+		$scope.chats.$add($scope.newChat).then(function(){
+			$scope.newChat = {name: "", avatar:"", text: ""} 
+		})
+		console.log($scope.chats)
+	}
+
+	$scope.removeChat = function(chat) {
+		$scope.chats.$remove(chat)
+	}
+
 	// $scope.todos = $firebaseArray(todosRef)
 	// $scope.newTodo = {text: "", completed: false}
 
