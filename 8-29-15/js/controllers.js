fda11f192e08a6777a7088de151d75ed51f6d07c
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-29 19:12:42 -0700

    font awesome working

--- 8-29-15/js/controllers.js
+++ 8-29-15/js/controllers.js
@@ -1,8 +1,14 @@
 app.controller('TodosController', function($scope, $firebaseArray){
 	var todosRef = new Firebase("https://radiant-torch-6315.firebaseio.com/list")
 	$scope.todos = $firebaseArray(todosRef)
+	$scope.newTodo = {text: "", completed: false}
 
+	$scope.addTodo = function() {
+		$scope.todos.$add($scope.newTodo).then(function(){
+			$scope.newTodo.text = ""		
+		})
 	
+	}
 
 	// $scope.todos.$add({text: "Walk Rusty", completed: false})
 
