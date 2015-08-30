bd52a92f55fa9e9c1a3eecabbc126cd1e4f76a58
Matthew Williams <mwilliamsnov6@gmail.com>
2015-08-29 17:30:09 -0700

    interate though todos

--- 8-29-15/js/controllers.js
+++ 8-29-15/js/controllers.js
@@ -1,6 +1,10 @@
-
 app.controller('TodosController', function($scope, $firebaseArray){
 	var todosRef = new Firebase("https://radiant-torch-6315.firebaseio.com/list")
 	$scope.todos = $firebaseArray(todosRef)
+
+	
+
+	// $scope.todos.$add({text: "Walk Rusty", completed: false})
+
 });
 
